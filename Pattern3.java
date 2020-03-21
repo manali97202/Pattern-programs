8380ff6f2ca00ece9aa2df88761972e29bc01b44
Manali Sharma <manali.sharma_cs17@gla.ac.in>
2020-03-21 17:58:32 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=t;i>0;i--){
+		    
+		    for(int j=0;j<i;j++){
+		        System.out.print("* ");
+		    }System.out.println();
+		}
+	
+	}
+}
