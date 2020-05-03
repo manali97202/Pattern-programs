a3381444310f80dc7216e71b8567ce60d0305495
Manali Sharma <manali.sharma_cs17@gla.ac.in>
2020-05-03 21:01:21 +0530

    Add files via upload

---
+++ pattern6.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+		    
+		    for(int j=n-i;j>1;j--){
+		        System.out.print(" ");
+		    }
+		    for(int k=0;k<=i;k++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		} 
+			for(int i=n-1;i>0;i--){
+			    System.out.print(" ");
+		    
+		    for(int j=n-i;j>1;j--){
+		        System.out.print(" ");
+		    }
+		    for(int k=0;k<i;k++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+		
+		
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+output:5
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+
+
+
+
+
