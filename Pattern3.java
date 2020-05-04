331a174919ce62bd5515686f671adb6174ba573b
Manali Sharma <manali.sharma_cs17@gla.ac.in>
2020-05-04 11:21:33 +0530

    Update Pattern3.java

--- Pattern3.java
+++ Pattern3.java
@@ -1,23 +1,23 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int t=sc.nextInt();
-		for(int i=t;i>0;i--){
-		    
-		    for(int j=0;j<i;j++){
-		        System.out.print("* ");
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+		    for(int j=n;j>i;j--){
+		        System.out.print("*");
 		    }System.out.println();
 		}
-	
+		
 	}
 }
+
+output:5
+*****
+****
+***
+**
+*
+
+
